b3f5be3586da8982e85ef3bc07d265b08b9c3acf
mikybars <mperezibars@gmail.com>
2023-08-22 08:04:37 +0200

    Add logging

--- src/main/java/com/github/mikybars/demo/spring/shell/MyCommands.java
+++ src/main/java/com/github/mikybars/demo/spring/shell/MyCommands.java
@@ -1,8 +1,13 @@
 package com.github.mikybars.demo.spring.shell;
 
+import org.jline.utils.Log;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 @ShellComponent
 public class MyCommands {
@@ -10,5 +15,39 @@ public class MyCommands {
     @ShellMethod(value = "Say hello", key = { "hello-world", "hw" })
     public String helloWorld(@ShellOption(defaultValue = "spring") String arg) {
         return "Hello World " + arg;
+    }
+
+    @ShellMethod(value = "Get contents of URL", key = { "web-get", "wg" })
+    public String webGet(String url) {
+        return getContentsOfUrlAsString(url);
+    }
+
+    @ShellMethod(value = "Save the contents of URL to file", key = { "web-save", "ws" })
+    public String webSave(String url, @ShellOption({ "out", "file" }) String file) {
+        String contents = getContentsOfUrlAsString(url);
+        try (PrintWriter out = new PrintWriter(file)) {
+            out.write(contents);
+        } catch (FileNotFoundException e) {
+            return "File not found: " + file;
+        }
+        return "Done.";
+    }
+
+    private String getContentsOfUrlAsString(String url) {
+        StringBuilder sb = new StringBuilder();
+        try {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    sb.append(inputLine);
+                }
+            }
+        } catch (MalformedURLException e) {
+            return "Bad URL";
+        } catch (IOException e) {
+            sb.append("ERROR: ").append(e.getMessage());
+            Log.error(e);
+        }
+        return sb.toString();
     }
 }
